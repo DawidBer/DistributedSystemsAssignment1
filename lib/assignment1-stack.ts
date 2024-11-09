a6646d9a9c5c3fc3603144ecc77804b10c81baa1
Dawid Beres <20098868@mail.wit.ie>
2024-11-09 15:05:25 +0100

    Added the setup for Auth API

--- lib/assignment1-stack.ts
+++ lib/assignment1-stack.ts
@@ -6,13 +6,44 @@ import * as custom from "aws-cdk-lib/custom-resources";
 import { generateBatch } from "../shared/util";
 import {movies, movieCasts} from "../seed/movies";
 import * as apig from "aws-cdk-lib/aws-apigateway";
-
+import { Aws } from "aws-cdk-lib";
+import { UserPool } from "aws-cdk-lib/aws-cognito";
 import { Construct } from 'constructs';
+import * as iam from "aws-cdk-lib/aws-iam";
+import * as node from "aws-cdk-lib/aws-lambda-nodejs";
 
 export class Assignment1Stack extends cdk.Stack {
+  private auth: apig.IResource;
+  private userPoolId: string;
+  private userPoolClientId: string;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const userPool = new UserPool(this, "UserPool", {
+      signInAliases: { username: true, email: true },
+      selfSignUpEnabled: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    this.userPoolId = userPool.userPoolId;
+
+    const appClient = userPool.addClient("AppClient", {
+      authFlows: { userPassword: true },
+    });
+
+    this.userPoolClientId = appClient.userPoolClientId;
+
+    const authApi = new apig.RestApi(this, "AuthServiceApi", {
+      description: "Authentication Service RestApi",
+      endpointTypes: [apig.EndpointType.REGIONAL],
+      defaultCorsPreflightOptions: {
+        allowOrigins: apig.Cors.ALL_ORIGINS,
+      },
+    });
+
+    this.auth = authApi.root.addResource("auth");
+    
     //Tables
     const moviesTable = new dynamodb.Table(this, "MoviesTable", {  //Movies tables in dynamodb aws
       billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
@@ -239,5 +270,36 @@ export class Assignment1Stack extends cdk.Stack {
     new cdk.CfnOutput(this, "Get Movie Cast members url", { value: getMovieCastMemberURL.url });
 
 
+  }
+
+  //Auth Method
+  private addAuthRoute(
+    resourceName: string,
+    method: string,
+    fnName: string,
+    fnEntry: string,
+    allowCognitoAccess?: boolean
+  ): void {
+    const commonFnProps = {
+      architecture: lambda.Architecture.ARM_64,
+      timeout: cdk.Duration.seconds(10),
+      memorySize: 128,
+      runtime: lambda.Runtime.NODEJS_18_X,
+      handler: "handler",
+      environment: {
+        USER_POOL_ID: this.userPoolId,
+        CLIENT_ID: this.userPoolClientId,
+        REGION: cdk.Aws.REGION
+      },
+    };
+    
+    const resource = this.auth.addResource(resourceName);
+    
+    const fn = new node.NodejsFunction(this, fnName, {
+      ...commonFnProps,
+      entry: `${__dirname}/../lambda/auth/${fnEntry}`,
+    });
+
+    resource.addMethod(method, new apig.LambdaIntegration(fn));
   }
 }
